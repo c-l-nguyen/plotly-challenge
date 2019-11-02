173ba1694e1cf49541544ee33a3727b5437f2530
c-l-nguyen <c.l.nguyen@icloud.com>
2019-11-02 00:18:33 -0500

    finished charts in part 1

--- Belly_Button_Diversity/static/js/app.js
+++ Belly_Button_Diversity/static/js/app.js
@@ -1,29 +1,63 @@
-function buildMetadata(sample) {
-
-  // @TODO: Complete the following function that builds the metadata panel
+async function buildMetadata(sample) {
 
   // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+  const url = "/metadata/" + sample;
+  let data = await d3.json(url);
 
-    // Use `.html("") to clear any existing metadata
+  // Use d3 to select the panel with id of `#sample-metadata`
+  let panel = d3.select('#sample-metadata');
 
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
+  // Use `.html("") to clear any existing metadata
+  panel.html("");
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+  // Use `Object.entries` to add each key and value pair to the panel
+  // Hint: Inside the loop, you will need to use d3 to append new
+  // tags for each key-value in the metadata.
+  let data_pairs = Object.entries(data);
+  // panel.append("text").text("a")
+  // data_pairs.forEach(pair => console.log(pair[0] + ": " + pair[1]))
+  // data_pairs.forEach(pair => panel.append(`$pair[0]: $pair[1]`));
+  data_pairs.forEach(pair => panel.append("text").text(pair[0] + ": " + pair[1] + "\n").append("br"));
+
+  // BONUS: Build the Gauge Chart
+  // buildGauge(data.WFREQ);
 }
 
-function buildCharts(sample) {
+async function buildCharts(sample) {
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  // Use `d3.json` to fetch the sample data for the plots
+  const url = "/samples/" + sample;
+  let data = await d3.json(url);
 
-    // @TODO: Build a Bubble Chart using the sample data
+  // Build a Pie Chart using top 10 values for sample_values, 
+  // otu_ids, and labels (10 each).
+  let sample_values = data.sample_values;
+  let otu_ids = data.otu_ids;
+  let otu_labels = data.otu_labels;
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
+  let pie_data = {
+    values: sample_values.slice(0,10),
+    labels: otu_ids.slice(0,10),
+    type: 'pie',
+    hovertext: otu_labels.slice(0,10)
+  }
+
+  Plotly.newPlot("pie", [pie_data]);
+  
+  // Build a Bubble Chart using the sample data
+  let bubble_data = {
+    x: otu_ids,
+    y: sample_values,
+    mode: 'markers',
+    marker: {
+              color: otu_ids, 
+              size: sample_values
+            },
+    text: otu_labels
+  }
+
+  Plotly.newPlot("bubble", [bubble_data]);
+
 }
 
 function init() {
