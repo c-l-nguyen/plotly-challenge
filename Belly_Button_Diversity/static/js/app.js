b271e3e5725a28cb9970d9f8c206a6b555c941a2
c-l-nguyen <c.l.nguyen@icloud.com>
2019-11-03 11:40:33 -0600

    refactored gauge chart code

--- Belly_Button_Diversity/static/js/app.js
+++ Belly_Button_Diversity/static/js/app.js
@@ -1,43 +1,23 @@
-async function buildMetadata(sample) {
+function buildGauge(wfreq){
 
-  // Use `d3.json` to fetch the metadata for a sample
-  const url = "/metadata/" + sample;
-  let data = await d3.json(url);
+  // original gauge plot which I think is easier to read
+  // let gauge_data = {
+  //   domain: { x: [0, 1], y: [0, 1] },
+  //   gauge: { axis: { range: [null, 9] } },
+	// 	value: wfreq,
+  //   title: { text: "Scrubs Per Week" },
+	// 	type: "indicator",
+	// 	mode: "gauge+number"
+  // }
+  // Plotly.newPlot("gauge", [gauge_data]);
 
-  // Use d3 to select the panel with id of `#sample-metadata`
-  let panel = d3.select('#sample-metadata');
-
-  // Use `.html("") to clear any existing metadata
-  panel.html("");
-
-  // Use `Object.entries` to add each key and value pair to the panel
-  // Hint: Inside the loop, you will need to use d3 to append new
-  // tags for each key-value in the metadata.
-  let data_pairs = Object.entries(data);
-  // panel.append("text").text("a")
-  // data_pairs.forEach(pair => console.log(pair[0] + ": " + pair[1]))
-  // data_pairs.forEach(pair => panel.append(`$pair[0]: $pair[1]`));
-  data_pairs.forEach(pair => panel.append("text").text(pair[0] + ": " + pair[1] + "\n").append("br"));
-
-  // BONUS: Build the Gauge Chart
-  // buildGauge(data.WFREQ);
-  let wfreq = data.WFREQ;
-
-  let gauge_data = {
-    domain: { x: [0, 1], y: [0, 1] },
-    gauge: { axis: { range: [null, 9] } },
-		value: wfreq,
-    title: { text: "Scrubs Per Week" },
-		type: "indicator",
-		mode: "gauge+number"
-  }
-
+  // pie chart converted to gauge chart
   let traceGauge = {
     type: 'pie',
     showlegend: false,
     hole: 0.4,
     rotation: 90,
-    values: [ 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180],
+    values: [180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180],
     text: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
     direction: 'clockwise',
     textinfo: 'text',
@@ -49,7 +29,21 @@ async function buildMetadata(sample) {
     }
   }
 
-  // needle
+  // the dot where the needle "originates"
+  let dot = {
+    type: 'scatter',
+    x:[0],
+    y:[0],
+    marker: {
+      size: 14,
+      color:'#850000'
+    },
+    showlegend: false
+  }
+
+  // the needle
+
+  // simple needle version
   // let degrees = 20, radius = .5;
   // let radians = degrees * Math.PI / 180 * wfreq;
   // let x = -1 * radius * Math.cos(radians);
@@ -72,27 +66,23 @@ async function buildMetadata(sample) {
   //   yaxis: {visible: false, range: [-1, 1]}
   // }
 
-  // var aX = 0.025 * Math.cos((degrees) * Math.PI / 180);
-  // var aY = 0.025 * Math.sin((degrees) * Math.PI / 180);
-  // var bX = -0.025 * Math.cos((degrees) * Math.PI / 180);
-  // var bY = -0.025 * Math.sin((degrees) * Math.PI / 180);
-
+  // triangular needle version
   let degrees = 180-(20 * wfreq);
   let radius = .5;
-  var radians = degrees * Math.PI / 180;
-  var aX = 0.025 * Math.cos((radians) * Math.PI / 180);
-  var aY = 0.025 * Math.sin((radians) * Math.PI / 180);
-  var bX = -0.025 * Math.cos((radians) * Math.PI / 180);
-  var bY = -0.025 * Math.sin((radians) * Math.PI / 180);
-  var cX = radius * Math.cos(radians);
-  var cY = radius * Math.sin(radians);
+  let radians = degrees * Math.PI / 180;
+  let aX = 0.025 * Math.cos((radians) * Math.PI / 180);
+  let aY = 0.025 * Math.sin((radians) * Math.PI / 180);
+  let bX = -0.025 * Math.cos((radians) * Math.PI / 180);
+  let bY = -0.025 * Math.sin((radians) * Math.PI / 180);
+  let cX = radius * Math.cos(radians);
+  let cY = radius * Math.sin(radians);
 
-  var path = 'M ' + aX + ' ' + aY +
+  let path = 'M ' + aX + ' ' + aY +
             ' L ' + bX + ' ' + bY +
             ' L ' + cX + ' ' + cY +
             ' Z';
 
-  var gaugeLayout = {
+  let gaugeLayout = {
     shapes:[{
         type: 'path',
         path: path,
@@ -107,20 +97,33 @@ async function buildMetadata(sample) {
                showgrid: false, range: [-1, 1]}
   };
 
-  // the dot where the needle "originates"
-  let dot = {
-    type: 'scatter',
-    x:[0],
-    y:[0],
-    marker: {
-      size: 14,
-      color:'#850000'
-    },
-    showlegend: false
+  Plotly.newPlot("gauge", [traceGauge, dot], gaugeLayout);
 }
 
-  // Plotly.newPlot("gauge", [gauge_data]);
-  Plotly.newPlot("gauge", [traceGauge, dot], gaugeLayout);
+async function buildMetadata(sample) {
+
+  // Use `d3.json` to fetch the metadata for a sample
+  const url = "/metadata/" + sample;
+  let data = await d3.json(url);
+
+  // Use d3 to select the panel with id of `#sample-metadata`
+  let panel = d3.select('#sample-metadata');
+
+  // Use `.html("") to clear any existing metadata
+  panel.html("");
+
+  // Use `Object.entries` to add each key and value pair to the panel
+  // Hint: Inside the loop, you will need to use d3 to append new
+  // tags for each key-value in the metadata.
+  let data_pairs = Object.entries(data);
+  // panel.append("text").text("a")
+  // data_pairs.forEach(pair => console.log(pair[0] + ": " + pair[1]))
+  // data_pairs.forEach(pair => panel.append(`$pair[0]: $pair[1]`));
+  data_pairs.forEach(pair => panel.append("text").text(pair[0] + ": " + pair[1] + "\n").append("br"));
+
+  // BONUS: Build the Gauge Chart
+  buildGauge(data.WFREQ);
+  
 }
 
 async function buildCharts(sample) {
