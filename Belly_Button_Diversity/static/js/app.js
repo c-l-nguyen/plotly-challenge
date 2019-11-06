69f8015fecdc4e07bf70b2ff5a354a82df906771
c-l-nguyen <c.l.nguyen@icloud.com>
2019-11-05 22:21:05 -0600

    added correctional weights to gauge ticker and turn off zoom for gauge

--- Belly_Button_Diversity/static/js/app.js
+++ Belly_Button_Diversity/static/js/app.js
@@ -22,8 +22,8 @@ function buildGauge(wfreq){
   // the dot where the needle "originates"
   let dot = {
     type: 'scatter',
-    x:[0],
-    y:[0],
+    x: [0],
+    y: [0],
     marker: {
       size: 14,
       color:'#850000'
@@ -33,7 +33,22 @@ function buildGauge(wfreq){
   }
 
   // the needle (triangular version)
-  let degrees = 180-(20 * wfreq);
+
+    // add weights to the degrees to correct needle
+  let weight = 0;
+  if (wfreq == 2 || wfreq == 3){
+    weight = 3;
+  }else if (wfreq == 4){
+    weight = 1;
+  }else if (wfreq == 5){
+    weight = -.5;
+  }else if (wfreq == 6){
+    weight = -2;
+  }else if (wfreq == 7){
+    weight = -3;
+  }
+
+  let degrees = 180-(20 * wfreq + weight); // 20 degrees for each of the 9 gauge sections
   let radius = .5;
   let radians = degrees * Math.PI / 180;
   let aX = 0.025 * Math.cos((radians) * Math.PI / 180);
@@ -43,6 +58,7 @@ function buildGauge(wfreq){
   let cX = radius * Math.cos(radians);
   let cY = radius * Math.sin(radians);
 
+  // draw the triangle
   let path = 'M ' + aX + ' ' + aY +
             ' L ' + bX + ' ' + bY +
             ' L ' + cX + ' ' + cY +
@@ -58,10 +74,18 @@ function buildGauge(wfreq){
           color: '#850000'
         }
       }],
-    xaxis: {zeroline:false, showticklabels:false,
-               showgrid: false, range: [-1, 1]},
-    yaxis: {zeroline:false, showticklabels:false,
-               showgrid: false, range: [-1, 1]}
+    xaxis: {zeroline:false, 
+            showticklabels:false,
+            showgrid: false, 
+            range: [-1, 1],
+            fixedrange: true
+          },
+    yaxis: {zeroline:false, 
+            showticklabels:false,
+            showgrid: false, 
+            range: [-1, 1],
+            fixedrange: true
+          }
   };
 
   Plotly.newPlot("gauge", [traceGauge, dot], gaugeLayout);
@@ -121,7 +145,7 @@ async function buildCharts(sample) {
     mode: 'markers',
     marker: {
               color: otu_ids, 
-              size: sample_values.map(d => d/1.2)
+              size: sample_values.map(d => d)
             },
     hovertext: otu_labels
   }
